9ba1ea6f3022ae9f4bfb3c2a2854db51d1a1f056
Anand <rautanand0311@gmail.com>
2020-07-12 23:47:25 +0530

    Updates

---
+++ precache-manifest.4766aeafa043dfa5b7abeefd4519191f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2820af168c611f7a66590e0f6d17c80c",
+    "url": "/myWebsite/index.html"
+  },
+  {
+    "revision": "539344054268f28ae61e",
+    "url": "/myWebsite/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0c620c891c33846db4f2",
+    "url": "/myWebsite/static/js/2.95a58a5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myWebsite/static/js/2.95a58a5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "539344054268f28ae61e",
+    "url": "/myWebsite/static/js/main.c3fd8acb.chunk.js"
+  },
+  {
+    "revision": "120e27a19c59655fe1f9",
+    "url": "/myWebsite/static/js/runtime-main.8e6b3816.js"
+  }
+]);
